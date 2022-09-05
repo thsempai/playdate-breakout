754d095bd5eed8ab56803ff1f7d63f80995044fb
Thomas Stassin <thomas.stassin@gsmproductions.com>
2022-09-05 16:35:56 +0200

    crank move the player

--- source/player.lua
+++ source/player.lua
@@ -1,4 +1,5 @@
 import "CoreLibs/sprites"
+import "CoreLibs/crank"
 import "game"
 
 local gfx <const> = playdate.graphics
@@ -14,21 +15,28 @@ function Player:init(x, y, speed)
     self:moveTo(x, y)
     self:setCollideRect(0, 0, image:getSize())
     self.speed = speed
+    self.crankMultiple = 10
 end
 
 function Player:update()
     Player.super:update(self)
-    if pd.buttonIsPressed(playdate.kButtonRight) then
-        self:moveBy(self.speed, 0)
-        if self.x >= getRightBorder() - self.width / 2 then
-            self:moveTo(getRightBorder() - self.width / 2, self.y)
-        end
+    if pd.isCrankDocked() then
+        if pd.buttonIsPressed(playdate.kButtonRight) then
+            self:moveBy(self.speed, 0)
 
-    elseif pd.buttonIsPressed(playdate.kButtonLeft) then
-        self:moveBy(-self.speed, 0)
-        if self.x <= getLeftBorder() + self.width / 2 then
-            self:moveTo(getLeftBorder() + self.width / 2, self.y)
+        elseif pd.buttonIsPressed(playdate.kButtonLeft) then
+            self:moveBy(-self.speed, 0)
+
         end
+    else
+        local dx = playdate.getCrankChange() / 359.9999
+        self:moveBy(self.speed * dx * self.crankMultiple, 0)
+    end
+
+    if self.x <= getLeftBorder() + self.width / 2 then
+        self:moveTo(getLeftBorder() + self.width / 2, self.y)
+    elseif self.x >= getRightBorder() - self.width / 2 then
+        self:moveTo(getRightBorder() - self.width / 2, self.y)
     end
 
 
